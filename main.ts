540a59568ec67d7d24987d37327b44dae97460e3
pmeinhardt <code@mnhrdt.io>
2025-07-27 00:54:58 +0200

    Use weather forecast to adjust recommendations 🌤️

--- main.ts
+++ main.ts
@@ -4,12 +4,18 @@ import { ChatAnthropic } from "@langchain/anthropic";
 import { MultiServerMCPClient } from "@langchain/mcp-adapters";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 
+function getenv(key: string): string {
+  const value = Deno.env.get(key);
+  if (!value) throw new Error(`Environment variable "${key}" is not set`);
+  return value;
+}
+
 if (import.meta.main) {
-  if (!Deno.env.has("ANTHROPIC_API_KEY")) {
-    throw new Error("ANTHROPIC_API_KEY is not set");
-  }
+  const ANTHROPIC_API_KEY = getenv("ANTHROPIC_API_KEY");
+  const OPENWEATHER_API_KEY = getenv("OPENWEATHER_API_KEY");
 
   const model = new ChatAnthropic({
+    apiKey: ANTHROPIC_API_KEY,
     model: "claude-3-7-sonnet-latest",
     temperature: 1.0,
   });
@@ -25,6 +31,18 @@ if (import.meta.main) {
         transport: "stdio",
         command: "docker",
         args: ["run", "--interactive", "--rm", "mcp/time"],
+      },
+      weather: {
+        transport: "stdio",
+        command: "docker",
+        args: [
+          "run",
+          "--interactive",
+          "--rm",
+          "-e",
+          `OPENWEATHER_API_KEY=${OPENWEATHER_API_KEY}`,
+          "mcp/openweather",
+        ],
       },
     },
   });
@@ -42,25 +60,32 @@ if (import.meta.main) {
         {
           role: "user",
           content: `
-          Please find and suggest 5 interesting events in Berlin this weekend.
-          Let me know when and where they take place, and what they are about.
-          Provide links to sources.
+            Please find and suggest 5 interesting events in Berlin this weekend.
+            Let me know when and where they take place, and what they are about.
+            Provide links to sources.
 
-          I enjoy:
+            I enjoy:
 
-          - Film and cinema, esp. smaller independent cinemas
-          - Art, primarily modern and contemporary art
-          - Design, graphic design, illustration, product and industrial design
-          - Music, mostly rock, metal, but also electronic music
-          - Bicycles and cycling
-          - Photography
-          - Science
+            - Film and cinema, esp. smaller independent cinemas
+            - Art, primarily modern and contemporary art
+            - Design, graphic design, illustration, product and industrial design
+            - Music, mostly rock, metal, but also electronic music
+            - Bicycles and cycling
+            - Bouldering and climbing
+            - Photography
+            - Science and technology
+            - Nature and the outdoors
 
-          Thank you!
-        `.replace(/^\s{10}/gm, ""),
+            Take into account the weather forecast for Berlin this weekend.
+            Include the forecast in your response.
+
+            If it is going to rain, prefer indoor activities.
+
+            Thank you!
+          `.replace(/^\s{12}/gm, "").trim(),
         },
       ],
-    });
+    }, { recursionLimit: 50 });
 
     const response = state.messages[state.messages.length - 1];
 
